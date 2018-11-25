54cec8f67c01457c5feb44c1d734eb463d0bd137
bdf2ch <bdf2ch@gmail.com>
2018-11-25 22:41:09 +0300

    temp

--- src/control-panel/applications/applications.controller.ts
+++ src/control-panel/applications/applications.controller.ts
@@ -1,27 +1,40 @@
 import { Controller, Post, Get, Body, Param, Query, Delete, Patch } from '@nestjs/common';
 import { ApplicationsService } from './applications.service';
-import { IPermission, IServerResponse } from '@kolenergo/lib';
+import { IPermission, IRole, IServerResponse } from '@kolenergo/lib';
 import { IApplication } from '@kolenergo/cp';
 
 @Controller('cp/applications')
 export class ApplicationsController {
-    constructor(private readonly applicationsService: ApplicationsService) {}
+  constructor(private readonly applicationsService: ApplicationsService) {
+  }
 
-    @Get('/')
-    async getApplications(): Promise<IServerResponse<IApplication[]>> {
-        const result = await this.applicationsService.getApplications();
-        return result;
-    }
+  @Get('/')
+  async getApplications(): Promise<IServerResponse<IApplication[]>> {
+    const result = await this.applicationsService.getApplications();
+    return result;
+  }
 
-    @Post('/permissions')
-    async addPermission(@Body() permission: IPermission): Promise<IServerResponse<IPermission>> {
-        const result = await this.applicationsService.addPermission(permission);
-        return result;
-    }
+  @Post('/roles')
+  async addRole(@Body() role: IRole): Promise<IServerResponse<IRole>> {
+    const result = await this.applicationsService.addRole(role);
+    return result;
+  }
 
-    @Patch('/permissions/:id')
-    async editPermission(@Body() permission: IPermission): Promise<IServerResponse<IPermission>> {
-        const result = await this.applicationsService.editPermission(permission);
-        return result;
-    }
+  @Patch('/roles/:id')
+  async editRole(@Body() role: IRole): Promise<IServerResponse<IRole>> {
+    const result = await this.applicationsService.editRole(role);
+    return result;
+  }
+
+  @Post('/permissions')
+  async addPermission(@Body() permission: IPermission): Promise<IServerResponse<IPermission>> {
+    const result = await this.applicationsService.addPermission(permission);
+    return result;
+  }
+
+  @Patch('/permissions/:id')
+  async editPermission(@Body() permission: IPermission): Promise<IServerResponse<IPermission>> {
+    const result = await this.applicationsService.editPermission(permission);
+    return result;
+  }
 }
--- src/control-panel/applications/applications.service.ts
+++ src/control-panel/applications/applications.service.ts
@@ -1,75 +1,112 @@
 import { Component } from '@nestjs/common';
 import { PostgresService } from '../../common/database/postgres.service';
-import { IPermission, IServerResponse } from '@kolenergo/lib';
+import { IPermission, IRole, IServerResponse } from '@kolenergo/lib';
 import { IAhoServerResponse } from '@kolenergo/aho';
 import { IApplication } from '@kolenergo/cp';
 
 @Component()
 export class ApplicationsService {
-    constructor(private readonly postgresService: PostgresService) {}
+  constructor(private readonly postgresService: PostgresService) {
+  }
 
-    /**
-     * Получение списка всех приложений
-     * @returns {Promise<>}
-     */
-    async getApplications(): Promise<IServerResponse<IApplication[]>> {
-        const result = await this.postgresService.query(
-            'get-applications',
-            `SELECT applications_get_all()`,
-            [],
-            'applications_get_all',
-        );
-        return result ? result : null;
-    }
+  /**
+   * Получение списка всех приложений
+   * @returns {Promise<>}
+   */
+  async getApplications(): Promise<IServerResponse<IApplication[]>> {
+    const result = await this.postgresService.query(
+      'get-applications',
+      `SELECT applications_get_all()`,
+      [],
+      'applications_get_all',
+    );
+    return result ? result : null;
+  }
 
-    /**
-     * Добавление нового права пользователя
-     * @param permission - Добавляемое право пользователя
-     */
-    async addPermission(permission: IPermission): Promise<IServerResponse<IPermission>> {
-        const result = await this.postgresService.query(
-            'add-permission',
-            'SELECT applications_add_permission($1, $2, $3)',
-            [
-                permission.applicationId,
-                permission.code,
-                permission.title,
-            ],
-            'applications_add_permission',
-        );
-        return result ? result : null;
-    }
+  /**
+   * Добавление новой роли пользователя
+   * @param role - Добавляемая роль пользователя
+   */
+  async addRole(role: IRole): Promise<IServerResponse<IRole>> {
+    const result = await this.postgresService.query(
+      'add-dole',
+      'SELECT applications_add_role($1, $2, $3)',
+      [
+        role.applicationId,
+        role.code,
+        role.title,
+      ],
+      'applications_add_role',
+    );
+    return result ? result : null;
+  }
 
-    /**
-     * Изменение права пользователя
-     * @param permission
-     */
-    async editPermission(permission: IPermission): Promise<IServerResponse<IPermission>> {
-        const result = await this.postgresService.query(
-            'edit-permission',
-            `SELECT applications_edit_permission($1, $2, $3)`,
-            [
-                permission.id,
-                permission.code,
-                permission.title,
-            ],
-            'applications_edit_permission',
-        );
-        return result ? result : null;
-    }
+  /**
+   * Изменение роли пользователя
+   * @param role - Изменяемая роль пользователя
+   */
+  async editRole(role: IRole): Promise<IServerResponse<IRole>> {
+    const result = await this.postgresService.query(
+      'edit-role',
+      `SELECT applications_edit_role($1, $2, $3)`,
+      [
+        role.id,
+        role.code,
+        role.title,
+      ],
+      'applications_edit_role',
+    );
+    return result ? result : null;
+  }
 
-    /**
-     * Получение данных для инициализации приложения
-     * @param userId - Идентификатор пользователя
-     * @param itemsOnPage - Количество заявок на странице
-     */
-    async getInitialData(userId: number, itemsOnPage: number): Promise<IServerResponse<IAhoServerResponse>> {
-        const result = await this.postgresService.query(
-            'aho-requests-get-initial-data',
-            `SELECT aho_requests_get_initial_data($1, $2)`,
-            [userId, itemsOnPage],
-            'aho_requests_get_initial_data',
-        );
-        return result;
-    }
+  /**
+   * Добавление нового права пользователя
+   * @param permission - Добавляемое право пользователя
+   */
+  async addPermission(permission: IPermission): Promise<IServerResponse<IPermission>> {
+    const result = await this.postgresService.query(
+      'add-permission',
+      'SELECT applications_add_permission($1, $2, $3)',
+      [
+        permission.applicationId,
+        permission.code,
+        permission.title,
+      ],
+      'applications_add_permission',
+    );
+    return result ? result : null;
+  }
+
+  /**
+   * Изменение права пользователя
+   * @param permission - Изменяемое право пользователя
+   */
+  async editPermission(permission: IPermission): Promise<IServerResponse<IPermission>> {
+    const result = await this.postgresService.query(
+      'edit-permission',
+      `SELECT applications_edit_permission($1, $2, $3)`,
+      [
+        permission.id,
+        permission.code,
+        permission.title,
+      ],
+      'applications_edit_permission',
+    );
+    return result ? result : null;
+  }
+
+  /**
+   * Получение данных для инициализации приложения
+   * @param userId - Идентификатор пользователя
+   * @param itemsOnPage - Количество заявок на странице
+   */
+  async getInitialData(userId: number, itemsOnPage: number): Promise<IServerResponse<IAhoServerResponse>> {
+    const result = await this.postgresService.query(
+      'aho-requests-get-initial-data',
+      `SELECT aho_requests_get_initial_data($1, $2)`,
+      [userId, itemsOnPage],
+      'aho_requests_get_initial_data',
+    );
+    return result;
+  }
 }
